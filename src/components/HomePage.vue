9621471f110dd87c4feb52a94e9606f2ae74fb42
krhistopher <krhistopher@weon.com.br>
2023-01-16 17:05:08 -0300

    ok finalizado

---
+++ src/components/HomePage.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="arrayObject">
+        <ListPerson :items="items"/>
+        <button type="button" @click="setItems"> Add New Item</button>
+    </div>
+    
+</template>
+
+<script>
+/* eslint-disable */ 
+import ListPerson from './ListPerson.vue';
+
+export default {
+    components: {
+        ListPerson
+    },
+    data(){
+        return {
+            items: [
+            {
+                ID: 0,
+                Nome: "Krhis",
+                Idade: 0,
+                Local: {
+                    Cidade: "Curitiba",
+                    Estado: "Paraná"
+                }
+            },
+            {
+                ID: 1,
+                Nome: "Krhiws",
+                Idade: 1,
+                Local: {
+                    Cidade: "Curitiba",
+                    Estado: "Paraná"
+                }
+            }
+            ]
+        }
+    },
+    methods: {
+        setItems(){
+            const newItem = 
+            {
+                ID: 2,
+                Nome: "Rafa Lindu",
+                Idade: 2,
+                Local: {
+                    Cidade: "Curitiba",
+                    Estado: "Paraná"
+                }
+            }
+
+            this.items.push(newItem)
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ListPerson.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="listAssembler">
+            <tr>
+                <td> ID </td>
+                <td> Nome </td>
+                <td> Idade </td>
+                <td> Cidade </td>
+                <td> Estado </td>
+            </tr>
+            <div v-for="value in items" :key="value.id">
+                <tr>
+                    <td> {{ value.ID }} </td>
+                    <td> {{ value.Nome }}</td>
+                    <td> {{ value.Idade }}</td>
+                    <td> {{ value.Local.Cidade }}</td>
+                    <td> {{ value.Local.Estado }}</td>
+    
+                </tr>
+            </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    /* eslint-disable */
+    props: {
+        items: {
+            type: Object
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+#listAssembler td, tr{
+    border: 1px solid black;
+    background: #acacac31;
+    padding: 10px;
+    
+}
+
+
+
+</style>
